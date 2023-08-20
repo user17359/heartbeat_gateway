b826e2aafbad7323c2ad8686184eb3b773d823ab
Ryszard Błażej <sapermysza@gmail.com>
2023-08-20 19:55:26 +0200

    Simple bluetooth scanning

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import typer
+import asyncio
+from bleak import BleakScanner
 
 app = typer.Typer()
 
 
 @app.command()
 def startup():
-    print(f'Hi')
+    print("Scanning for BLE devices...")
+    asyncio.run(scan())
 
 
-@app.command()
-def display_info():
-    print(f'Hi')
-
+async def scan():
+    devices = await BleakScanner.discover()
+    for d in devices:
+        print(d.details)
 
 if __name__ == '__main__':
     app()
