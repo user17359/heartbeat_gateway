22c8b9cca451d96f2173758c5c3dcebda8cf6e1d
Ryszard Błażej <sapermysza@gmail.com>
2023-08-26 21:05:44 +0200

    Added scan for services

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 import typer
 import asyncio
-from bleak import BleakScanner
+from bleak import BleakScanner, BleakClient
+from rich import print
 
 app = typer.Typer()
+uuid = []
 
 
 @app.command()
-def startup():
-    print("Scanning for BLE devices...")
-    asyncio.run(scan())
+def app():
+    load_uuid()
+    print("Scanning for :sparkles:BLE devices:sparkles:")
+    asyncio.run(scan_app())
 
 
-async def scan():
-    devices = await BleakScanner.discover()
-    for d in devices:
-        print(d.details)
+def load_uuid():
+    file = open("uuid.txt", "r")
+    uuid.append(file.read())
+    file.close()
+
+
+async def scan_app():
+    devices = await BleakScanner.discover(return_adv=True)
+    for device, adv_data in devices.values():
+        if adv_data.service_uuids == uuid:
+            print("Found mobile app :flushed:")
+            # TODO: connection with mobile app
+            async with BleakClient(device.address) as client:
+                svcs = client.services
+                print("Services :eyes:")
+                for service in svcs:
+                    print(service)
+
+
 
 if __name__ == '__main__':
     app()
