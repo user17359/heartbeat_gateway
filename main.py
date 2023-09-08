9848ffa6ef31d1180dcdd9bb42ae3a7aeb1a3082
Ryszard Błażej <sapermysza@gmail.com>
2023-09-08 19:44:23 +0200

    Added choosing sensor and listening time

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import pandas as pd
 import typer
 import asyncio
 from bleak import BleakScanner, BleakClient
 from rich import print
+from typing_extensions import Annotated
 
 from data_view import DataView
 
@@ -16,56 +18,80 @@ NOTIFY_CHARACTERISTIC_UUID = (
     "34800002-7185-4d5d-b431-630e7050e8f0"
 )
 
+sensor_options = ["acc", "gyro", "mag"]
+
+options_dict = {
+    "acc": "/Meas/Acc/13",
+    "gyro": "/Meas/Gyro/13",
+    "mag": "/Meas/Magn/13"
+}
+
+
+def sensor_callback(value: str):
+    if value not in sensor_options:
+        raise typer.BadParameter("Invalid sensor")
+    return value
+
 
 @app.command()
-def app():
-    print("Scanning for :sparkles:BLE devices:sparkles:")
-    asyncio.run(scan_app())
+def app(
+    connection_time: float,
+    sensor: Annotated[
+            str, typer.Option(help="Sensor from which data will be registered.", prompt="Choose sensor (acc/gyro/mag)",
+                              callback=sensor_callback)
+        ],
+):
+    asyncio.run(async_app(connection_time, options_dict[sensor]))
 
 
-"""
-def load_uuid():
-    file = open("uuid.txt", "r")
-    uuid.append(file.read())
-    file.close()
-"""
+async def async_app(time, sensor):
+    address = await scan_sensor()
+    if address is not None:
+        await timed_connection(address, time, sensor)
 
 
 async def notification_handler(sender, data):
     """Simple notification handler which prints the data received."""
     d = DataView(data)
     # Dig data from the binary
-    msg = "Data: ts: {}, ax: {}, ay: {}, az: {}".format(d.get_uint_32(2),
-                                                        d.get_float_32(6),
-                                                        d.get_float_32(10),
-                                                        d.get_float_32(14))
-    # queue message for later consumption
+    timestamp = d.get_uint_32(2)
+    x = d.get_float_32(6)
+    y = d.get_float_32(10)
+    z = d.get_float_32(14)
+
+    msg = "timestamp: {}, x: {}, y: {}, z: {}".format(timestamp, x, y, z)
     print(msg)
 
 
-async def scan_app():
+async def scan_sensor():
+    print("Scanning for :sparkles: BLE devices:sparkles: ")
     devices = await BleakScanner.discover(return_adv=True)
     for device, adv_data in devices.values():
         if adv_data.local_name is not None and name_key in adv_data.local_name:
             print("Found Movesense device :flushed:")
-            # TODO: connection with mobile app
-            async with BleakClient(device.address) as client:
-                svcs = client.services
-                print("Services :eyes:")
-                for service in svcs:
-                    print(service)
-                    print("with characteristics :open_mouth::")
-                    for char in service.characteristics:
-                        print(char)
-                    print("")
-                print("Enabling notifications")
-                await client.start_notify(NOTIFY_CHARACTERISTIC_UUID, notification_handler)
-                print("Subscribing datastream")
-                await client.write_gatt_char(WRITE_CHARACTERISTIC_UUID,
-                                             bytearray([1, 99]) + bytearray("/Meas/Acc/13", "utf-8"), response=True)
+            return device.address
 
-                await asyncio.sleep(15.0)
 
+async def timed_connection(address, time, sensor):
+    client = BleakClient(address)
+    try:
+        await client.connect()
+        print("Enabling notifications")
+        await client.start_notify(NOTIFY_CHARACTERISTIC_UUID, notification_handler)
+        print("Subscribing datastream")
+        await client.write_gatt_char(WRITE_CHARACTERISTIC_UUID,
+                                     bytearray([1, 99]) + bytearray(sensor, "utf-8"), response=True)
+
+        await asyncio.sleep(time)
+
+        print("Unsubscribe")
+        await client.write_gatt_char(WRITE_CHARACTERISTIC_UUID, bytearray([2, 99]), response=True)
+        print("Stop notifications")
+        await client.stop_notify(NOTIFY_CHARACTERISTIC_UUID)
+    except Exception as e:
+        print(e)
+    finally:
+        await client.disconnect()
 
 if __name__ == '__main__':
-    app()
+    typer.run(app)
