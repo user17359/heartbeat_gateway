4dc05885b25459383fc19ce61114e095efcaf14a
Ryszard Błażej <sapermysza@gmail.com>
2023-09-08 20:09:39 +0200

    Added saving data to .csv and more user friendly UI

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import asyncio
 from bleak import BleakScanner, BleakClient
 from rich import print
 from typing_extensions import Annotated
+from rich.progress import track
 
 from data_view import DataView
 
@@ -26,6 +27,10 @@ options_dict = {
     "mag": "/Meas/Magn/13"
 }
 
+df = pd.DataFrame(columns=["timestamp", "x", "y", "z"])
+
+state = {"verbose": False}
+
 
 def sensor_callback(value: str):
     if value not in sensor_options:
@@ -35,13 +40,18 @@ def sensor_callback(value: str):
 
 @app.command()
 def app(
-    connection_time: float,
-    sensor: Annotated[
+        connection_time: float,
+        sensor: Annotated[
             str, typer.Option(help="Sensor from which data will be registered.", prompt="Choose sensor (acc/gyro/mag)",
                               callback=sensor_callback)
         ],
+        verbose: bool = False
 ):
+    if verbose:
+        state["verbose"] = True
     asyncio.run(async_app(connection_time, options_dict[sensor]))
+    df.to_csv('result.csv', index=False)
+    print("Data saved to [blue]results.csv[blue] :floppy_disk:")
 
 
 async def async_app(time, sensor):
@@ -59,8 +69,10 @@ async def notification_handler(sender, data):
     y = d.get_float_32(10)
     z = d.get_float_32(14)
 
+    df.loc[len(df)] = [timestamp, x, y, z]
     msg = "timestamp: {}, x: {}, y: {}, z: {}".format(timestamp, x, y, z)
-    print(msg)
+    if state["verbose"]:
+        print(msg)
 
 
 async def scan_sensor():
@@ -76,22 +88,30 @@ async def timed_connection(address, time, sensor):
     client = BleakClient(address)
     try:
         await client.connect()
-        print("Enabling notifications")
+        if state["verbose"]:
+            print("Enabling notifications")
         await client.start_notify(NOTIFY_CHARACTERISTIC_UUID, notification_handler)
-        print("Subscribing datastream")
+        if state["verbose"]:
+            print("Subscribing datastream")
         await client.write_gatt_char(WRITE_CHARACTERISTIC_UUID,
                                      bytearray([1, 99]) + bytearray(sensor, "utf-8"), response=True)
+        if state["verbose"]:
+            await asyncio.sleep(time)
+        else:
+            for value in track(range(100), description="Listening... "):
+                await asyncio.sleep(time / 100)
 
-        await asyncio.sleep(time)
-
-        print("Unsubscribe")
+        if state["verbose"]:
+            print("Unsubscribe")
         await client.write_gatt_char(WRITE_CHARACTERISTIC_UUID, bytearray([2, 99]), response=True)
-        print("Stop notifications")
+        if state["verbose"]:
+            print("Stop notifications")
         await client.stop_notify(NOTIFY_CHARACTERISTIC_UUID)
     except Exception as e:
         print(e)
     finally:
         await client.disconnect()
+
 
 if __name__ == '__main__':
     typer.run(app)
