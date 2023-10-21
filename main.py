eeda251fa3c7999a977f30b5a6573a6c5df2b7d1
user17359 <blazejryszard@gmail.com>
2023-10-21 15:46:01 +0200

    Added BLE advertisment as peripheral to conncet with app

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
+#!/usr/bin/env python3
+import numpy
 import pandas as pd
 import typer
 import asyncio
 from rich import print
 from typing_extensions import Annotated
 
-from bluetooth.sensor.scan_sensor import scan_sensor
-from bluetooth.sensor.timed_connection import timed_connection
+from bt.sensor.scan_sensor import scan_sensor
+from bt.sensor.timed_connection import timed_connection
+from bt.gateway.services.test_service import HeartRateService
 from data.avaiable_sensors import sensor_options
 
-app = typer.Typer()
+from bluez_peripheral.util import *
+from bluez_peripheral.advert import Advertisement
+from bluez_peripheral.agent import NoIoAgent
+from bluez_peripheral.gatt.service import ServiceCollection
+
+typer_app = typer.Typer()
 
 df = pd.DataFrame
 
@@ -21,11 +29,12 @@ def bad_sensor_callback(value: str):
     return value
 
 
-@app.command()
-def app(
+@typer_app.command()
+def timed(
         connection_time: float,
         sensor: Annotated[
-            str, typer.Option(help="Sensor from which data will be registered.", prompt="Choose sensor (acc/gyro/mag/ecg)",
+            str, typer.Option(help="Sensor from which data will be registered.",
+                              prompt="Choose sensor (acc/gyro/mag/ecg)",
                               callback=bad_sensor_callback)
         ],
         verbose: bool = False
@@ -37,16 +46,43 @@ def app(
         df = pd.DataFrame(columns=["timestamp", "val"])
     else:
         df = pd.DataFrame(columns=["timestamp", "x", "y", "z"])
-    asyncio.run(async_app(connection_time, sensor))
+    asyncio.run(async_timed(connection_time, sensor))
     df.to_csv('result.csv', index=False)
     print("Data saved to [blue]results.csv[blue] :floppy_disk:")
 
 
-async def async_app(time, sensor):
+async def async_timed(time, sensor):
     address = await scan_sensor()
     if address is not None:
         await timed_connection(address, time, sensor, df, state)
 
 
+@typer_app.command()
+def startup():
+    asyncio.run(async_startup())
+
+
+async def async_startup():
+    bus = await get_message_bus()
+
+    service = HeartRateService()
+    await service.register(bus)
+
+    agent = NoIoAgent()
+    await agent.register(bus)
+
+    adapter = await Adapter.get_first(bus)
+
+    print("Start of advertisement :loudspeaker:")
+    advert = Advertisement("Heartbeat gateway 2809", ["180D"], 0x0000, 30)
+    await advert.register(bus, adapter)
+
+    for i in range(6):
+        # Update the heart rate.
+        service.update_heart_rate(120)
+        # Handle dbus requests.
+        await asyncio.sleep(5)
+
+
 if __name__ == '__main__':
-    typer.run(app)
+    typer_app()
