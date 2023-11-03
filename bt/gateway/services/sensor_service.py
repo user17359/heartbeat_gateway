04c2e0f1515f43310230a912b4914a8da107148d
user17359 <blazejryszard@gmail.com>
2023-11-03 13:50:21 +0100

    Added timed connection at start of measurement

--- bt/gateway/services/sensor_service.py
+++ bt/gateway/services/sensor_service.py
@@ -1,3 +1,5 @@
+import asyncio
+import pandas as pd
 from datetime import datetime
 
 from bluez_peripheral.gatt.service import Service
@@ -5,12 +7,16 @@ from bluez_peripheral.gatt.characteristic import characteristic, CharacteristicF
 
 import json
 
+from bt.sensor.timed_connection import timed_connection
+
 
 class SensorService(Service):
-
     current_sensor = ""
+    run_at = None
+    end_at = None
     start_event = None
     end_event = None
+    df = pd.DataFrame(columns=["timestamp", "x", "y", "z"])
 
     def __init__(self, scheduler):
         # Base 16 service UUID, This should be a primary service.
@@ -19,9 +25,16 @@ class SensorService(Service):
 
     def start_measurement(self):
         print("Measuring...")
+        duration = (self.end_at - self.run_at).total_seconds()
+        asyncio.run(timed_connection(self.current_sensor,
+                                     duration,
+                                     "acc",
+                                     self.df,
+                                     {"verbose": True}))
 
     def end_measurement(self):
         print("End of measurement")
+        self.df.to_csv('result.csv', index=False)
 
     @characteristic("18c7e933-73cf-4d47-9973-51a53f0fec4e", CharFlags.WRITE_WITHOUT_RESPONSE)
     def new_measurement(self, options):
@@ -32,21 +45,20 @@ class SensorService(Service):
         data = json.loads(value)
         self.current_sensor = data["mac"]
         now = datetime.now()
-        run_at = datetime(now.year,
-                          now.month,
-                          now.day,
-                          data["startHour"],
-                          data["startMinute"])
-        stop_at = datetime(now.year,
-                           now.month,
-                           now.day,
-                           data["endHour"],
-                           data["endMinute"])
+        self.run_at = datetime(now.year,
+                               now.month,
+                               now.day,
+                               data["startHour"],
+                               data["startMinute"])
+        self.end_at = datetime(now.year,
+                               now.month,
+                               now.day,
+                               data["endHour"],
+                               data["endMinute"])
 
-        self.start_event = self.scheduler.enterabs(run_at.timestamp(),
+        self.start_event = self.scheduler.enterabs(self.run_at.timestamp(),
                                                    10,
                                                    self.start_measurement())
-        self.end_event = self.scheduler.enterabs(stop_at.timestamp(),
+        self.end_event = self.scheduler.enterabs(self.end_at.timestamp(),
                                                  10,
                                                  self.end_measurement())
-
