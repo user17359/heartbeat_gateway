52ac058b4ca7039e2ccd0e944d604ef08468e159
Ryszard Błażej <sapermysza@gmail.com>
2024-03-25 00:28:28 +0100

    Minor fixes

--- bt/sensor/supported/Movesense/movesense_connection.py
+++ bt/sensor/supported/Movesense/movesense_connection.py
@@ -26,8 +26,10 @@ class MovesenseConnection(Connection):
             return ["timestamp", "value"]
         elif unit == "hr":
             return ["timestamp", "hr", "rr"]
-        else:
+        elif unit == "imu":
             return ["timestamp", "accX", "accY", "accZ", "gyroX", "gyroY", "gyroZ", "magX", "magY", "magZ"]
+        else:
+            raise Exception()
 
     async def start_connection(self, data_storage, state, client, service, units):
         try:
@@ -36,29 +38,31 @@ class MovesenseConnection(Connection):
                     await client.write_gatt_char(ECG_PROBING_UUID, probing_to_diff[unit["probing"]])
                     diff = int.from_bytes((await client.read_gatt_char(ECG_PROBING_UUID))[:1], byteorder='little')
 
-                    async def handler(_, data):
+                    async def ecg_handler(_, data):
                         d = DataView(data)
-                        await self.notification_handler.notification_handler_ecg(_, d, data_storage[unit["name"]], state,
+                        await self.notification_handler.notification_handler_ecg(_, d, data_storage["ecg"], state,
                                                                                  service, diff)
-                    await client.start_notify(ECG_VOLTAGE_UUID, handler)
+                    await client.start_notify(ECG_VOLTAGE_UUID, ecg_handler)
                     self.subscriptions.append(ECG_VOLTAGE_UUID)
                 elif unit["name"] == "hr":
-                    async def handler(_, data):
+                    async def hr_handler(_, data):
                         d = DataView(data)
-                        await self.notification_handler.notification_handler_hr(_, d, data_storage[unit["name"]], state,
+                        await self.notification_handler.notification_handler_hr(_, d, data_storage["hr"], state,
                                                                                 service)
-                    await client.start_notify(HR_UUID, handler)
+                    await client.start_notify(HR_UUID, hr_handler)
                     self.subscriptions.append(HR_UUID)
-                else:
+                elif unit["name"] == "imu":
                     await client.write_gatt_char(MOVEMENT_PROBING_UUID, probing_to_diff[unit["probing"]])
                     diff = int.from_bytes((await client.read_gatt_char(MOVEMENT_PROBING_UUID))[:1], byteorder='little')
 
-                    async def handler(_, data):
+                    async def imu_handler(_, data):
                         d = DataView(data)
-                        await self.notification_handler.notification_handler_imu(_, d, data_storage[unit["name"]], state,
+                        await self.notification_handler.notification_handler_imu(_, d, data_storage["imu"], state,
                                                                                  service, diff)
-                    await client.start_notify(MOVEMENT_UUID, handler)
+                    await client.start_notify(MOVEMENT_UUID, imu_handler)
                     self.subscriptions.append(MOVEMENT_UUID)
+                else:
+                    raise Exception()
 
             if state["verbose"]:
                 print("Subscribing datastream")
@@ -71,3 +75,4 @@ class MovesenseConnection(Connection):
             print('Cancelling' + str(sub))
             await client.stop_notify(sub)
         print("Unsubscribing")
+        self.subscriptions.clear()
